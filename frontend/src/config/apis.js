8bb154ee21a15e904ab2178509e37b35a1c007df
vasu <vasumandaviya05@gmail.com>
2025-01-06 16:41:56 +0530

    Commit1

--- frontend/src/config/apis.js
+++ frontend/src/config/apis.js
@@ -1,9 +1,5 @@
-// const localpath = "http://127.0.0.1:3001";
-//const localpath = "https://real-time-chat-git-master-mvl-vasus-projects.vercel.app";
+
 const localpath = "https://real-time-chat-backend-seven.vercel.app";
-// const localpath = `http://${window.location.hostname}:3001`;
-// const localpath = "http://192.168.148.222:3001";
-// const localpath = "http://192.168.1.100:3001";
 
 const api_paths = {
 
